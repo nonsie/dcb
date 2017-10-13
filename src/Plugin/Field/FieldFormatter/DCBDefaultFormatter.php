cc16179fbf4499f634de6a2c5c9448c8cf314e4a
Gary Morse <gmorse81@gmail.com>
2017-10-13 12:51:01 -0400

    some comments.

--- src/Plugin/Field/FieldFormatter/DCBDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/DCBDefaultFormatter.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * @File: Plugin implementation of the 'dcb_default' formatter.
- */
-
 namespace Drupal\dcb\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldDefinitionInterface;
@@ -28,14 +24,35 @@ class DCBDefaultFormatter extends FormatterBase implements ContainerFactoryPlugi
 
   /**
    * @var \Drupal\dcb\Controller\DCBRegionController
+   *   Injected Region controller.
    */
   protected $DCBRegionController;
 
+  /**
+   * DCBDefaultFormatter constructor.
+   *
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   * @param array $settings
+   * @param string $label
+   * @param string $view_mode
+   * @param array $third_party_settings
+   * @param \Drupal\dcb\Controller\DCBRegionController $DCBRegionController
+   */
   public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, DCBRegionController $DCBRegionController) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
     $this->DCBRegionController = $DCBRegionController;
   }
 
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition){
     return new static(
       $plugin_id,
@@ -64,7 +81,7 @@ class DCBDefaultFormatter extends FormatterBase implements ContainerFactoryPlugi
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
     /**
-     * @var  $delta
+     * @var mixed $delta
      * @var FieldItemListInterface $item
      */
     foreach ($items as $delta => $item) {
